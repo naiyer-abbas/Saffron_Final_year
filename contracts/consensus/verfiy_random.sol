41988595cdcabf6d95cc758c27035886a710b942
Mohammad Haseeb <mohammad.haseeb@xivtech.io>
2022-03-28 16:05:09 +0530

    10 th commit

--- contracts/consensus/verfiy_random.sol
+++ contracts/consensus/verfiy_random.sol
@@ -11,11 +11,12 @@ contract verify_random
     }
 
     person [] private v_members;
-
+    uint [] pred_arr;
+    uint [] Hsh;
     uint global_id = 0;
 
     mapping (address => bool) private verifiers;
-
+    mapping (uint => person) private registered;
 
     function ver_register() public {
         require(verifiers[msg.sender] == false);
@@ -23,6 +24,7 @@ contract verify_random
         person memory p;
         p.id = global_id ++;
         p.rep_score = 1;
+        registered[p.id] = p;
         v_members.push(p);
     }
 
@@ -34,14 +36,31 @@ contract verify_random
         uint new_range = num_max - num_min;
         return (((random_num - 0) * new_range) / old_range) + num_min;
     }
-
+    function fill_pred_arr () internal returns(uint)
+    {
+        uint track;
+        for(uint i = 0; i < v_members.length; i++)
+        {
+            track = v_members[i].rep_score;
+            for(uint j = 0; j < track ; j++)
+            {
+                pred_arr.push(v_members[i].id);
+            }
+        }
+        uint ran = generate_random(pred_arr.length-1, 0);
+        return pred_arr[ran];
+    }
     function select_5_people() internal
     {
         require(v_members.length >= 5);
-        uint [] storage pred_arr;
+        uint T = 5;
         for(uint i = 0; i < v_members.length; i++)
         {
-            
+            Hsh[i] = 0;
+        }
+        while(T != 0)
+        {
+
         }
     }
 
